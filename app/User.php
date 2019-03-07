5c7fc090305ee095f79e1dac40c8010682986653
David García <cdgarcia65@misena.edu.co>
2019-03-07 11:50:15 -0500

    Eloquent Relationships

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get all of the task for the user.
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
