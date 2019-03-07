b92fd8b89a429c49bda8ea9babfbc39cf6d92173
David García <cdgarcia65@misena.edu.co>
2019-03-07 12:08:41 -0500

    Adding tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,4 +15,23 @@ class TaskController extends Controller
     {
         $this->middleware('auth');
     }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255'
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('tasks.index');
+    }
 }
